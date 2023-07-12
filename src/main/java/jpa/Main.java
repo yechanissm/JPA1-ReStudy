4490aa6657f667d890cf9445a8e8a180429e2740
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-07-12 17:12:53 +0900

    JPA는 트랜잭션이 중요

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -2,22 +2,26 @@ package jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         Member member = new Member();
 
         member.setId(1L);
         member.setName("lee");
 
-        entityManager.persist(member);
+        em.persist(member);
 
-        entityManager.close();
+        tx.commit();
+        em.close();
 
         emf.close();
     }
