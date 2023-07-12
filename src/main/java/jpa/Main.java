29db75ed2fb6704fd0b05872ca1312493b714da9
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-07-12 16:59:30 +0900

    엔티티매니저로 JPA 동작 확인하기

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -1,7 +1,17 @@
 package jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.close();
+
+        emf.close();
     }
 }
