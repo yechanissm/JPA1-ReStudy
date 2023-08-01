b526f57f8938a76fc9816e2352634e38866e9709
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-08-01 11:17:50 +0900

    구현 클래스마다 테이블 전략

--- src/main/java/jpa/Item.java
+++ src/main/java/jpa/Item.java
@@ -3,7 +3,7 @@ package jpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn
 public abstract class Item {
 
