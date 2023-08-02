af4f50b072b1b3a4aa548c2d02d0f703bb93c233
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-08-02 19:54:13 +0900

    영속성 전이 : 저장

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -16,29 +16,15 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Parent parent = new Parent();
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("user1");
-            member1.setTeam(team);
-            em.persist(member1);
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member2 = new Member();
-            member2.setUsername("user2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
+            em.persist(parent);
 
 
             tx.commit();
