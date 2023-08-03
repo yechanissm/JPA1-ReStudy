b8ab26c632870b796afa314ffc1b39a7b6d7ae1a
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-08-03 19:40:33 +0900

    값 타입 컬렉션 수정

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -35,10 +35,17 @@ public class Main {
             Member findMember = em.find(Member.class, member.getId());
 
             System.out.println("==============시작===========");
-            List<Address> addressHistory = findMember.getAddressHistory();
-            for (Address address : addressHistory) {
-                System.out.println(address.getCity());
-            }
+
+            //homecity -> newcity
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newcity", a.getStreet(), a.getZipcode()));
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
 
             tx.commit();
